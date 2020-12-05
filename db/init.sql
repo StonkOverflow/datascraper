27ae9718fa87b6832f542ffb85c9a118f628d2f3
kevin meyers <kevinm1776@gmail.com>
2020-12-05 10:55:03 -0500

    Moved Dockerfile into its stock-socket.

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE datascraper-test;
-use datascraper-test;
+CREATE DATABASE datascraper_test;
+use datascraper_test;
 
 CREATE TABLE darkpools (
   pid INT,
@@ -10,6 +10,6 @@ CREATE TABLE darkpools (
 INSERT INTO darkpools
   (pid, last_dir, last)
 VALUES
-  (1, greenBg, 1.1934),
-  (1, redBG, 1.1935);
+  (1, "greenBg", 1.1934),
+  (1, "redBG", 1.1935);
 
